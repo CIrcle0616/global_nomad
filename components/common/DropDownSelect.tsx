d7209db2f7b687345d0a3ef2ee67b0978a496ebd
zeon0xx0 <jihyon5615@naver.com>
2025-05-28 14:58:10 +0900

    refactor: 드롭다운, 인풋 재사용성 확장

--- components/common/DropDownSelect.tsx
+++ components/common/DropDownSelect.tsx
@@ -4,21 +4,35 @@ import Input from './Input';
 import Image from 'next/image';
 import ArrowIcon from '@/public/ic_arrow.svg';
 import CheckIcon from '@/public/ic_check.svg';
+import DatePicker from './DatePicker';
 
 type DropdownSelectProps = {
-  options: string[];
+  options?: string[];
   selected?: string;
   onSelect: (value: string) => void;
   placeholder: string;
+  type?: 'list' | 'datepicker';
 };
 
-export default function DropdownSelect({ onSelect, selected, placeholder, options }: DropdownSelectProps) {
+export default function DropdownSelect({
+  onSelect,
+  selected,
+  placeholder,
+  options,
+  type = 'list',
+}: DropdownSelectProps) {
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null); //외부 클릭 감지
 
   const toggle = () => setIsOpen(prev => !prev);
   const handleSelect = (value: string) => {
     onSelect(value);
+    setIsOpen(false);
+  };
+
+  const handleSelectDate = (date: Date | undefined) => {
+    if (!date) return;
+    onSelect(date.toLocaleDateString());
     setIsOpen(false);
   };
 
@@ -33,7 +47,7 @@ export default function DropdownSelect({ onSelect, selected, placeholder, option
   }, []);
 
   return (
-    <div ref={dropdownRef} className="relative itmes-center max-w-[800px] w-full max-w-xs">
+    <div ref={dropdownRef} className="relative">
       <div onClick={toggle}>
         <Input
           value={selected || ''}
@@ -51,18 +65,30 @@ export default function DropdownSelect({ onSelect, selected, placeholder, option
       </div>
 
       {isOpen && (
-        <ul className="absolute top-full left-0 mt-2 w-full border border-gray-200 rounded-md bg-white shadow-md z-50">
-          {options.map(option => (
-            <li key={option} onClick={() => handleSelect(option)} className="px-2 py-2 cursor-pointer text-gray-900">
-              <div className="group">
-                <div className="flex items-center gap-2 w-full py-2 px-3 rounded-[6px] group-hover:bg-black group-hover:text-white transition-colors duration-200">
-                  <Image src={CheckIcon} alt="체크" className="hidden group-hover:inline w-4 h-4" />
-                  <span>{option}</span>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+        <div className="absolute top-full left-0 mt-2 w-full border border-gray-200 rounded-md bg-white shadow-md z-50">
+          {type === 'list' && options && (
+            <ul>
+              {options.map(option => (
+                <li
+                  key={option}
+                  onClick={() => handleSelect(option)}
+                  className="px-2 py-2 cursor-pointer text-gray-900"
+                >
+                  <div className="group">
+                    <div className="flex items-center gap-2 w-full py-2 px-3 rounded-[6px] group-hover:bg-black group-hover:text-white transition-colors duration-200">
+                      <Image src={CheckIcon} alt="체크" className="hidden group-hover:inline w-4 h-4" />
+                      <span>{option}</span>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+
+          {type === 'datepicker' && (
+            <DatePicker selected={selected ? new Date(selected) : undefined} onSelect={handleSelectDate} />
+          )}
+        </div>
       )}
     </div>
   );
