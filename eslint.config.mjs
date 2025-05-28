f0c4e7247b8888afc21180805d09da89eaf55003
Circle0616 <happy_faces@naver.com>
2025-05-28 14:30:30 +0900

    fix: 로컬에서 develop 충돌사항 정리

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,7 @@ export default [
   {
     rules: {
       camelcase: 'warn', // 변수명은 camelCase로 작성 권장
+
       '@typescript-eslint/no-unused-vars': 'warn', // TypeScript 변수 미사용 경고
       'no-redeclare': 'error', // 변수 중복 선언 방지
       'no-dupe-keys': 'error', // 객체에서 key 중복 방지
