087667b06e9cca55419980a406a5c4f9ea8a503c
jeonsooyoung <peace2332@naver.com>
2025-05-30 01:23:40 +0900

    feat: common button 공통 컴포넌트 수정2

--- components/common/CommonButton.tsx
+++ components/common/CommonButton.tsx
@@ -7,7 +7,25 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   children?: React.ReactNode;
   width?: string;
   className?: string;
+  type?: 'button' | 'submit' | 'reset';
 }
+
+function CommonButton({
+  size = 'L',
+  variant = 'primary',
+  children,
+  width,
+  className,
+  type = 'button',
+  ...rest
+}: Props) {
+  return (
+    <button type={type} className={clsx(SIZE_MAP[size], VARIANT_MAP[variant ?? 'primary'], width, className)} {...rest}>
+      {children}
+    </button>
+  );
+}
+
 const SIZE_MAP: {
   L: string;
   M: string;
@@ -24,13 +42,7 @@ const VARIANT_MAP: {
   primary:
     'border border-nomad-black bg-nomad-black text-white font-bold text-center disabled:border-gray-600 disabled:bg-gray-600 disabled:text-white',
   secondary:
-    'border border-nomad-black text-nomad-black font-bold text-center  disabled:border-gray-600 disabled:bg-gray-600 disabled:text-white',
+    'border border-nomad-black text-nomad-black font-bold text-center disabled:border-gray-600 disabled:bg-gray-600 disabled:text-white',
 };
-function commonButton({ size = 'L', variant = 'primary', children, width, className, ...rest }: Props) {
-  return (
-    <button className={clsx(SIZE_MAP[size], VARIANT_MAP[variant ?? 'primary'], width, className)} {...rest}>
-      {children}
-    </button>
-  );
-}
-export default commonButton;
+
+export default CommonButton;
