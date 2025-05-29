7ffa6a19fc9ef558d417e9297b78792746d04eab
nayejin <nayejin259@gmail.com>
2025-05-29 19:54:32 +0900

    fix: api 요청 주소 변경, teamId 제거거

--- services/myReservations.ts
+++ services/myReservations.ts
@@ -1,22 +1,22 @@
 import { fetchWrapper } from './fetchWrapper';
 
 // 내 예약 리스트 조회
-export function getMyReservations(teamId: string, cursorId?: number, size?: number, status?: string) {
+export function getMyReservations(cursorId?: number, size?: number, status?: string) {
   const query = new URLSearchParams({
     ...(cursorId ? { cursorId: String(cursorId) } : {}),
     ...(size ? { size: String(size) } : {}),
     ...(status ? { status } : {}),
   });
 
-  return fetchWrapper(`/${teamId}/my-reservations?${query}`, 'GET');
+  return fetchWrapper(`/my-reservations?${query}`, 'GET');
 }
 
 // 내 예약 수정(취소)
-export function patchMyReservations(teamId: string, reservationId: number, body: { status: 'canceled' }) {
-  return fetchWrapper(`/${teamId}/my-reservations/${reservationId}`, 'PATCH', body);
+export function patchMyReservations(reservationId: number, body: { status: 'canceled' }) {
+  return fetchWrapper(`/my-reservations/${reservationId}`, 'PATCH', body);
 }
 
 // 내 예약 리뷰 작성
-export function postMyReservations(teamId: string, reservationId: number, body: { rating: number; content: string }) {
-  return fetchWrapper(`/${teamId}/my-reservations/${reservationId}/reviews`, 'POST', body);
+export function postMyReservations(reservationId: number, body: { rating: number; content: string }) {
+  return fetchWrapper(`/my-reservations/${reservationId}/reviews`, 'POST', body);
 }
--- services/users.ts
+++ services/users.ts
@@ -1,24 +1,24 @@
 import { fetchWrapper } from './fetchWrapper';
 
 // 회원가입
-export function signUp(teamId: string, body: { email: string; nickname: string; password: string }) {
-  return fetchWrapper(`/${teamId}/users`, 'POST', body);
+export function signUp(body: { email: string; nickname: string; password: string }) {
+  return fetchWrapper(`/users`, 'POST', body);
 }
 
 // 내 정보 조회
-export function getUserMe(teamId: string) {
-  return fetchWrapper(`/${teamId}/users/me`, 'GET');
+export function getUserMe() {
+  return fetchWrapper(`/users/me`, 'GET');
 }
 
 // 내 정보 수정
-export function patchUserMe(teamId: string, body: { nickname: string; profileImageUrl: string; newPassword: string }) {
-  return fetchWrapper(`/${teamId}/users/me`, 'PATCH', body);
+export function patchUserMe(body: { nickname: string; profileImageUrl: string; newPassword: string }) {
+  return fetchWrapper(`/users/me`, 'PATCH', body);
 }
 
 // 프로필 이미지  url 생성
-export function postProfileImg(teamId: string, image: File) {
+export function postProfileImg(image: File) {
   const formData = new FormData();
   formData.append('image', image);
 
-  return fetchWrapper(`/${teamId}/users/me/image`, 'POST', formData);
+  return fetchWrapper(`/users/me/image`, 'POST', formData);
 }
