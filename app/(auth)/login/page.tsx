354e84634ae48df5b16c6fc674ee3ce469c906f8
Circle0616 <happy_faces@naver.com>
2025-05-30 21:56:49 +0900

    feat: 로그인 페이지 작성 (기능 동작 확인 close #23)

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,31 @@
+import LoginForm from '@/components/domain/login/LoginForm';
+import HrWithText from '@/components/domain/login/HrWithText';
+import AuthLogo from '@/components/domain/login/AuthLogo';
+import KakaoOauthLink from '@/components/domain/login/KakaoOauthLink';
+import LinkToAuth from '@/components/domain/login/LinkToSign';
+
 export default function LoginPage() {
-  return <h1>로그인 페이지를 작성해주세요</h1>
-};
+  return (
+    <main className="w-full max-w-[640px] h-screen pt-[110px] px-3 mx-auto">
+      <div className="relative w-[270px] md:w-[340px] h-[154px] md:h-[192px] mx-auto mb-6">
+        <AuthLogo />
+      </div>
+
+      <div className="mx-auto mb-6">
+        <LoginForm />
+      </div>
+
+      <div className="flex justify-center text-gray-900 mb-10">
+        <LinkToAuth text="회원이 아니신가요?" href="/signup" childrenText="회원가입하기" />
+      </div>
+
+      <div>
+        <HrWithText text="SNS 계정으로 로그인하기" />
+
+        <div className="relative w-12 h-12 mx-auto">
+          <KakaoOauthLink />
+        </div>
+      </div>
+    </main>
+  );
+}
