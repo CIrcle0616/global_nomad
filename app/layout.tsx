d3e308f6690a075c55ab0ba81e5e9ae41e730b4c
원현 <81631735+CIrcle0616@users.noreply.github.com>
2025-05-30 10:55:05 +0900

    Revert "docs: 필요없는 주석 삭제"
    This reverts commit d8855e58bea5bd3879cec17f23054955e5ab8170.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="ko">
       <body>
         {children}
+        {/* 전역 모달 컴포넌트 위치 */}
         <OneButtonModal />
       </body>
     </html>
