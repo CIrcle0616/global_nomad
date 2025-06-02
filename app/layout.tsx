9ca9d46d70367179fb06725581bd565a20325dcb
parkchanyoung0710 <call4892@naver.com>
2025-06-02 13:58:05 +0900

    fix: 변수명 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <Providers>
           {children}
           {/* 전역 모달 컴포넌트 위치 */}
-          <OneButtonModal />
+          <GlobalModal />
         </Providers>
       </body>
     </html>
