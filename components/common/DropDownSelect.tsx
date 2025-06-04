fbf029ee9214b62b0e30eb3e501ed507aaab7e5c
zeon0xx0 <jihyon5615@naver.com>
2025-06-04 12:35:43 +0900

    refactor: dropdownSelect 및 input 컴포넌트 리팩토링

--- components/common/DropDownSelect.tsx
+++ components/common/DropDownSelect.tsx
@@ -1,24 +1,33 @@
 'use client';
 import { useEffect, useRef, useState } from 'react';
 import Input from './Input';
-import Image from 'next/image';
-import ArrowIcon from '@/public/ic_arrow.svg';
-import CheckIcon from '@/public/ic_check.svg';
+import DatePicker from './DatePicker';
+import DropdownMenu from './DropDown';
 
 type DropdownSelectProps = {
   options: string[];
   selected?: string;
   onSelect: (value: string) => void;
   placeholder: string;
+  type?: 'list' | 'datepicker';
+  icon?: React.ReactNode;
 };
 
-export default function DropdownSelect({ onSelect, selected, placeholder, options }: DropdownSelectProps) {
+export default function DropdownSelect({
+  onSelect,
+  selected,
+  placeholder,
+  options,
+  type = 'list',
+  icon,
+}: DropdownSelectProps) {
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null); //외부 클릭 감지
 
   const toggle = () => setIsOpen(prev => !prev);
-  const handleSelect = (value: string) => {
-    onSelect(value);
+  const handleSelectDate = (date: Date | undefined) => {
+    if (!date) return;
+    onSelect(date.toLocaleDateString());
     setIsOpen(false);
   };
 
@@ -32,37 +41,34 @@ export default function DropdownSelect({ onSelect, selected, placeholder, option
     return () => document.removeEventListener('mousedown', handleClickOutside);
   }, []);
 
+  if (type === 'list' && options) {
+    return (
+      <DropdownMenu
+        options={options}
+        onSelect={onSelect}
+        trigger={
+          <Input
+            value={selected || ''}
+            placeholder={placeholder}
+            onChange={() => {}}
+            readOnly
+            icon={{ element: icon }}
+          />
+        }
+      ></DropdownMenu>
+    );
+  }
+
   return (
-    <div ref={dropdownRef} className="relative itmes-center max-w-[800px] w-full max-w-xs">
+    <div ref={dropdownRef} className="relative w-full">
       <div onClick={toggle}>
-        <Input
-          value={selected || ''}
-          placeholder={placeholder}
-          onChange={() => {}}
-          readOnly
-          icon={
-            <Image
-              src={ArrowIcon}
-              alt="아래방향 화살표"
-              className={`w-4 h-4 transition-transform duration-200 ${isOpen ? 'rotate-180' : 'rotate-0'}`}
-            />
-          }
-        />
+        <Input value={selected || ''} placeholder={placeholder} onChange={() => {}} readOnly icon={{ element: icon }} />
       </div>
 
       {isOpen && (
-        <ul className="absolute top-full left-0 mt-2 w-full border border-gray-200 rounded-md bg-white shadow-md z-50">
-          {options.map(option => (
-            <li key={option} onClick={() => handleSelect(option)} className="px-2 py-2 cursor-pointer text-gray-900">
-              <div className="group">
-                <div className="flex items-center gap-2 w-full py-2 px-3 rounded-[6px] group-hover:bg-black group-hover:text-white transition-colors duration-200">
-                  <Image src={CheckIcon} alt="체크" className="hidden group-hover:inline w-4 h-4" />
-                  <span>{option}</span>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+        <div className="absolute top-full left-0 mt-2 w-full border border-gray-200 rounded-md bg-white shadow-md z-50">
+          <DatePicker selected={selected ? new Date(selected) : undefined} onSelect={handleSelectDate} />
+        </div>
       )}
     </div>
   );
