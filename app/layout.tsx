213d047a0b11891aaeb11163de1b591e9a6a9749
nayejin <nayejin259@gmail.com>
2025-06-15 21:42:23 +0900

    refactor: 카카오맵 스크립트 추가/ MapFooter에 주소복사, 길찾기 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,18 @@ import '@/app/globals.css';
 import GlobalModal from '@/components/common/modal/GlobalModal';
 import { ReactNode } from 'react';
 import { Providers } from '../lib/provider';
+import Script from 'next/script';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
+      <head>
+        <Script
+          src="https://dapi.kakao.com/v2/maps/sdk.js?appkey=ee8b727eb9bd5a77a22b851eacd5d101&autoload=false&libraries=services"
+          strategy="beforeInteractive"
+        />
+      </head>
+
       <body>
         <Providers>
           {children}
