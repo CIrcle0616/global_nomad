a0fd40eaf1a3159fcf8e7b674ffa67dc251d4fcd
Circle0616 <happy_faces@naver.com>
2025-06-17 13:47:34 +0900

    fix: 기타 수정사항 반영

--- app/profile/info/page.tsx
+++ app/profile/info/page.tsx
@@ -1,18 +1,5 @@
-import { HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query';
-import { getUserMe } from '@/services/users';
 import MyInfoForm from '@/components/domain/profile/MyInfoForm';
 
 export default async function MyInfoPage() {
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ['myInfo'],
-    queryFn: () => getUserMe(),
-  });
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <MyInfoForm />;
-    </HydrationBoundary>
-  );
+  return <MyInfoForm />;
 }
