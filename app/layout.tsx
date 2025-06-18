ff06d4ce83e7ef9e4bd1401282eb74ccca7212cb
Circle0616 <happy_faces@naver.com>
2025-06-18 14:44:47 +0900

    fix: 레이아웃 정리

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,27 +3,22 @@ import GlobalModal from '@/components/common/modal/GlobalModal';
 import { Providers } from '@/lib/provider';
 import Script from 'next/script';
 import { ReactNode } from 'react';
-import { Providers } from '../lib/provider';
-import GNB from '@/components/common/gnb/Gnb';
-import Footer from '@/components/common/Footer';
 import { Toaster } from 'react-hot-toast';
-
-
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
       <body>
-
         <Providers>
-          <GNB />
           {children}
           {/* 전역 모달 컴포넌트 위치 */}
           <GlobalModal />
-          <Footer />
+          <Script
+            src="https://dapi.kakao.com/v2/maps/sdk.js?appkey=ee8b727eb9bd5a77a22b851eacd5d101&autoload=false&libraries=services"
+            strategy="beforeInteractive"
+          />
           <Toaster position="top-center" reverseOrder={false} />
         </Providers>
-
       </body>
     </html>
   );
