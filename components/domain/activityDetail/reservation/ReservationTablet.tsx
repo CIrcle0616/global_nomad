978f0f31d8a6fa6a4028044b59bc316c71b80f98
zeon0xx0 <jihyon5615@naver.com>
2025-06-18 17:14:13 +0900

    feat: 날짜, 시간 시간 지나면 선택하지 못하도록, 예약 가능 시간 점으로 표시

--- components/domain/activityDetail/reservation/ReservationTablet.tsx
+++ components/domain/activityDetail/reservation/ReservationTablet.tsx
@@ -21,6 +21,11 @@ interface ReservationTabletProps {
   pricePerPerson: number;
   availableTimes: { startTime: string; endTime: string }[];
   loading?: boolean;
+  availableDates: string[];
+  scheduleData?: {
+    date: string;
+    times: { startTime: string; endTime: string; id: number }[];
+  }[];
 }
 
 export default function ReservationTablet({
@@ -30,8 +35,10 @@ export default function ReservationTablet({
   onCountChange,
   onReserve,
   pricePerPerson,
+  availableDates,
   loading = false,
   availableTimes,
+  scheduleData,
 }: ReservationTabletProps) {
   const { openModal } = useModalStore();
   const isReservable = !!state.date && state.time;
@@ -41,6 +48,8 @@ export default function ReservationTablet({
       onReserve();
     }
   };
+  // console.log('[Tablet] scheduleData:', scheduleData);
+  console.log('[ReservationTablet] scheduleData 전달 전:', scheduleData);
 
   return (
     <>
@@ -63,7 +72,9 @@ export default function ReservationTablet({
                   onTimeChange,
                   onReserve,
                   availableTimes,
+                  availableDates,
                   loading,
+                  scheduleData,
                 })
               }
               className="text-black hover:underline cursor-pointer text-sm"
--- components/domain/activityDetail/reservation/parts/DateSelector.tsx
+++ components/domain/activityDetail/reservation/parts/DateSelector.tsx
@@ -5,12 +5,13 @@ import DatePicker from '@/components/common/DatePicker';
 interface DateSelectorProps {
   date: Date | undefined;
   onSelect: (date: Date | undefined) => void;
+  availableDates: string[];
 }
 
-export default function DateSelector({ date, onSelect }: DateSelectorProps) {
+export default function DateSelector({ date, onSelect, availableDates }: DateSelectorProps) {
   return (
     <div className="flex justify-center">
-      <DatePicker selected={date} onSelect={onSelect} />
+      <DatePicker selected={date} onSelect={onSelect} availableDates={availableDates} />
     </div>
   );
 }
