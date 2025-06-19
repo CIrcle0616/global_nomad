800faaa71f70f419aae0216ee2698ffba811065a
parkchanyoung0710 <call4892@naver.com>
2025-06-19 22:58:54 +0900

    fix: gnb 최신 내용 반영 및 레이아웃 조정

--- components/common/gnb/Gnb.tsx
+++ components/common/gnb/Gnb.tsx
@@ -11,20 +11,21 @@ export default function GNB() {
   const { user } = useUserStore();
 
   return (
-    <header className="w-full px-2 border-b py-[10px]">
-      <div className="mx-auto flex justify-between items-center pl-4 sm:px-6 lg:px-[110px] max-w-[1440px]">
+    <header className="fixed bg-white z-50 left-0 top-0 w-full px-2 border-b py-[10px]">
+      <div className="w-full max-w-[1200px] mx-auto px-4 md:px-6 lg:px-0 flex justify-between items-center h-[60px]">
         <Link href={'/'} className="flex items-center gap-2">
-          <Image src={Logo} alt="로고" width={130} className="sm:w-[172px]" />
+          <Image src={Logo} alt="로고" width={150} className="sm:w-[172px]" />
         </Link>
 
         {/* 알림 버튼 */}
         {user ? (
-          <div className="flex items-center mt-2  gap-1 sm:gap-2 ml-auto">
+          <div className="flex gap-4 lg:gap-[30px] items-center">
             <AlarmButton />
+            <div className="h-4 w-px bg-gray-300" />
             <ProfileButton />
           </div>
         ) : (
-          <div className="flex gap-[30px] ml-auto">
+          <div className="flex gap-[30px] items-center">
             <Link
               href={'/login'}
               className="text-md-medium text-black transition ease-in-out hover:font-bold hover:scale-105"
--- components/layout/ProfileLayout.tsx
+++ components/layout/ProfileLayout.tsx
@@ -5,7 +5,7 @@ import SideProfile from '@/components/layout/SideProfile';
 export default function ProfileLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="flex flex-1 md:pt-6 lg:pt-20">
+      <div className="flex-1 pt-[72px] md:pt-[72px] lg:pt-[120px]">
         <div className="w-full max-w-[1440px] mx-auto mt-5 flex px-4 sm:px-6 lg:px-[110px] gap-6">
           <div className="hidden md:block md:w-[251px] lg:w-[384px] shrink-0">
             <SideProfile />
