ab4bf99bb8e2ede1dca597705fc393e19f8dfd60
zeon0xx0 <jihyon5615@naver.com>
2025-06-21 02:41:15 +0900

    refactor: 예약 현황 기능 최종 보완 및 체험 상세 예약 파일 삭제

--- app/(main)/profile/schedule/ScheduleClientInner.tsx
+++ app/(main)/profile/schedule/ScheduleClientInner.tsx
@@ -3,23 +3,19 @@
 import ScheduleName from '@/components/domain/schedule/ScheduleName';
 import { getMyActivities } from '@/services/myActivities';
 import { useQuery } from '@tanstack/react-query';
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 import MyReservationCalendar from './calendar/MyReservationCalendar';
 import EmptyState from '@/components/empty/EmptyState';
+import SkeletonCalendar from '@/components/skeleton/SkeletonCalendar';
 
 export default function ScheduleClientInner() {
   const [selectedId, setSelectedId] = useState<number | null>(null);
   const { data: activities, isLoading } = useQuery({
     queryKey: ['activities'],
-    queryFn: () => getMyActivities({}),
+    queryFn: () => getMyActivities({ size: 1000 }),
   });
 
   const activityList = useMemo(() => activities?.activities ?? [], [activities]);
-  useEffect(() => {
-    if (activityList.length > 0 && selectedId === null) {
-      setSelectedId(activityList[0].id);
-    }
-  }, [activityList, selectedId]);
 
   return (
     <div className="mb-6 ml-4">
@@ -28,15 +24,15 @@ export default function ScheduleClientInner() {
       </div>
 
       {isLoading ? (
-        <div>불러오는 중...</div>
+        <SkeletonCalendar />
       ) : activityList.length === 0 ? (
         <EmptyState />
-      ) : selectedId !== null ? (
+      ) : (
         <>
-          <ScheduleName activityList={activityList} selectedId={selectedId} onSelectedId={setSelectedId} />
-          <MyReservationCalendar activityId={selectedId} />
+          <ScheduleName activityList={activityList} selectedId={selectedId ?? -1} onSelectedId={setSelectedId} />
+          <MyReservationCalendar activityId={selectedId ?? 0} />
         </>
-      ) : null}
+      )}
     </div>
   );
 }
