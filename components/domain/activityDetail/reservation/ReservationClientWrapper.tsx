d407a41b4187f9ec0b6f2ed3d05c050fc48be286
JjinJjin <81224667+NaYejin94@users.noreply.github.com>
2025-06-20 15:14:04 +0900

    refactor: 내 체험일 경우 width 변경 (#151)
    Co-authored-by: nayejin <nayejin259@gmail.com>
    Co-authored-by: 원현 <81631735+CIrcle0616@users.noreply.github.com>

--- components/domain/activityDetail/reservation/ReservationClientWrapper.tsx
+++ components/domain/activityDetail/reservation/ReservationClientWrapper.tsx
@@ -1,17 +1,23 @@
 'use client';
 
-import useUserStore from '@/store/useUserStore';
 import ReservationSection from '@/app/(main)/activities/[activityId]/ReservationSection';
+import useIsAuthor from '@/hooks/useIsAuthor';
 
 export default function ReservationClientWrapper({ activityUserId }: { activityUserId: number }) {
-  const { user } = useUserStore();
-  const isAuthor = user?.id !== activityUserId;
+  const isAuthor = useIsAuthor(activityUserId);
 
-  if (!isAuthor) return null;
+  if (isAuthor) return null;
 
   return (
-    <div>
-      <ReservationSection />
+    <div className="ml-6">
+      <div
+        className="
+              fixed bottom-0 left-0 w-full z-10 bg-white border-t shadow-[0_-4px_10px_rgba(0,0,0,0.1)]
+              md:sticky md:top-0 md:left-auto md:w-auto md:shadow-none
+            "
+      >
+        <ReservationSection />
+      </div>
     </div>
   );
 }
