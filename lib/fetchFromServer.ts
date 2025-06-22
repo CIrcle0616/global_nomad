eacc6a1716cd8f138fd380657f053d17a9a36d62
zeon0xx0 <jihyon5615@naver.com>
2025-06-23 01:11:57 +0900

    refactor: url 변경

--- lib/fetchFromServer.ts
+++ lib/fetchFromServer.ts
@@ -5,7 +5,8 @@ export async function fetchFromServer(path: string, options: RequestInit = {}) {
   const cookieStore = await cookies();
   const accessToken = cookieStore.get('accessToken')?.value;
 
-  const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
+  //url 이후에 바꿀 것
+  const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'https://sp-globalnomad-api.vercel.app/14-2';
 
   const res = await fetch(`${baseUrl}${path}`, {
     ...options,
