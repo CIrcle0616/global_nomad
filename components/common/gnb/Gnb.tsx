87caadafba56a0b4972c972c478b8f46921d56b9
DAON <peace2332@naver.com>
2025-06-23 17:22:46 +0900

    style: gnb, 메인, 회원가입 페이지 UI 수정 (#159)

--- components/common/gnb/Gnb.tsx
+++ components/common/gnb/Gnb.tsx
@@ -19,21 +19,21 @@ export default function GNB() {
   const isLoggedOut = !isLoggedIn && !user;
 
   return (
-    <header className="fixed bg-white z-50 left-0 top-0 w-full px-2 border-b py-[10px]">
-      <div className="w-full max-w-[1200px] mx-auto px-4 md:px-6 lg:px-0 flex justify-between items-center h-[60px]">
-        <Link href={'/'} className="flex items-center gap-2">
-          <Image src={Logo} alt="로고" width={150} className="sm:w-[172px]" />
+    <header className="fixed bg-white z-50 left-0 top-0 w-full px-4 border-b py-[20px]">
+      <div className="w-full max-w-[1200px] mx-auto flex justify-between items-center">
+        <Link href={'/'}>
+          <Image src={Logo} alt="로고" width={172} className="sm:w-[172px]" />
         </Link>
 
         {/* 알림 버튼 */}
         {!isLoggedOut ? (
-          <div className="flex gap-4 lg:gap-[30px] items-center">
+          <div className="flex gap-[12px] md:gap-[25px] items-center">
             <AlarmButton />
             <div className="h-4 w-px bg-gray-300" />
             <ProfileButton />
           </div>
         ) : (
-          <div className="flex gap-[30px] items-center">
+          <div className="flex gap-[25px] items-center">
             <Link
               href={'/login'}
               className="text-md-medium text-black transition ease-in-out hover:font-bold hover:scale-105"
